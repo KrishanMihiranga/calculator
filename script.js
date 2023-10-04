4a21b6656498bcf868987eb2ad0fde5c4034a539
Krishan <xp3cto95@gmail.com>
2023-10-04 05:35:40 +0530

    functions

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 
+let txt ='';
 $('button').on('click', (event) => {
-   alert('button clicked!');
-});
+   try {
+     if (event.target.innerText == '=') {
+       txt = $('.display-input').val();
+       txt = String(eval(txt));
+       $('.display-input').val(txt);
+     } else if (event.target.innerText == 'C') {
+       txt = '';
+       $('.display-input').val(txt);
+     }
+     else if ($(event.target).closest('button').attr('value') == 'del') {
+       txt = txt.substring(0, txt.length-1);
+       $('.display-input').val(txt);
+     }
+     else {
+       txt += event.target.innerText;
+       $('.display-input').val(txt);
+     }
+   } catch (error) {
+      $('.display-input').val('o_O');
+   }
+ });
+ 
